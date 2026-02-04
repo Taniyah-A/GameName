6738322b7a5f2a718dd75643bc9999d0add929af
zerg26 <eric.misiak.26@gmail.com>
2026-02-03 19:07:43 -0500

    hopefullfixed

---
+++ Mouse Game/Assets/player/player_movement.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(PlayerInput))]
+
+public class PlayerController : MonoBehaviour
+{
+
+    
+
+    [Header("Movement")]
+    [SerializeField] float speed = 6f;
+    [SerializeField] float smoothturn = 0.1f;
+
+    float turnsmoothVelocity;
+
+    [Header("pysics")]
+    [SerializeField] float gravity = -19.62f; // Double Earth gravity feels better in games
+    [SerializeField] float jumpHeight = 2f;
+
+
+    private Vector3 playerVelocity; // Tracks vertical speed (gravity/jumping)
+    private bool isGrounded;
+
+    [Header("Camera")]
+
+    public Transform cam;
+
+
+    public CharacterController controller;
+    private PlayerInput playerInput;
+    private InputAction moveAction;
+    private InputAction lookAction;
+    private InputAction jumpAction;
+
+    private Vector2 moveInput;
+    private Vector2 lookInput;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        playerInput = GetComponent<PlayerInput>();
+
+        // Get actions directly from the PlayerInput component
+        moveAction = playerInput.actions["Move"];
+        lookAction = playerInput.actions["Look"];
+        jumpAction = playerInput.actions["Jump"];
+    }
+
+
+
+
+    private void Update() {
+        isGrounded = controller.isGrounded;
+        if (isGrounded && playerVelocity.y < 0)
+        {
+
+            playerVelocity.y = -2f;
+        }
+
+        Move();
+        ApplyGravityAndJump();
+    }
+
+    private void Move() {
+        Vector2 input = moveAction.ReadValue<Vector2>();
+        Vector3 move = new Vector3(input.x, 0f, input.y).normalized;
+
+        if (move.magnitude >= 0.1f) {
+            float targAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y,  targAngle, ref turnsmoothVelocity, smoothturn);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            Vector3 movedir = Quaternion.Euler(0, targAngle, 0) * Vector3.forward;
+
+            controller.Move(movedir.normalized * speed * Time.deltaTime);
+        }
+        
+    }
+
+    private void ApplyGravityAndJump()
+    {
+
+        if (jumpAction.triggered && isGrounded)
+        {
+
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+
+        playerVelocity.y += gravity * Time.deltaTime;
+
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
